887cf4933d79feda8b3f74b32666d73c91873bab
Michael Schuenck <michaelss@gmail.com>
2015-05-12 10:54:29 -0300

    Refactoring LinkTagLib.groovy.

--- grails-app/taglib/LinkTagLib.groovy
+++ grails-app/taglib/LinkTagLib.groovy
@@ -1,31 +1,24 @@
 import org.apache.shiro.SecurityUtils
 
-class LinkTagLib {
-
-	static namespace = "shiro"
+class LinkTagLib extends ShiroBaseTagLib {
 
 	def link = { attrs, body ->
-		String controller = attrs.controller ?: (attrs.url?.controller ?: controllerName)
-		String action = attrs.action ?: (attrs.url?.action ?: actionName)
-
-		if (SecurityUtils.subject.isPermitted("${controller}:${action}")) {
-			out << g.link([action: attrs.action,
-				controller: attrs.controller,
-				resource: attrs.resource,
-				namespace: attrs.namespace,
-				plugin: attrs.plugin,
-				elementId: attrs.elementId,
-				id: attrs.id,
-				fragment: attrs.fragment,
-				mapping: attrs.mapping,
-				params: attrs.params,
-				uri: attrs.uri,
-				relativeUri: attrs.relativeUri,
-				url: attrs.url,
-				absolute: attrs.absolute,
-				base: attrs.base,
-				event: attrs.event
-				], body())
-		}
+        protectedTag(attrs, body, g.link([action: attrs.action,
+			controller: attrs.controller,
+			resource: attrs.resource,
+			namespace: attrs.namespace,
+			plugin: attrs.plugin,
+			elementId: attrs.elementId,
+			id: attrs.id,
+			fragment: attrs.fragment,
+			mapping: attrs.mapping,
+			params: attrs.params,
+			uri: attrs.uri,
+			relativeUri: attrs.relativeUri,
+			url: attrs.url,
+			absolute: attrs.absolute,
+			base: attrs.base,
+			event: attrs.event
+			], body()))
 	}
 }
